0daffbd509147915f9e6a538bfbf0f110ce6cf96
harshil62-cell <harshil2461@gmail.com>
2025-07-12 15:24:08 +0530

    Minor fixes

--- queues/geminiQueue.js
+++ queues/geminiQueue.js
@@ -3,7 +3,8 @@ const Redis=require('ioredis');
 
 const connection = new Redis(process.env.REDIS_URL, {
   maxRetriesPerRequest: null,
-});;
+  tls: {}, // ✅ Required for rediss://
+});
 
 const geminiQueue=new Queue('gemini',{connection});
 
--- testredis.js
+++ testredis.js
@@ -1,10 +1,11 @@
 const Redis = require('ioredis');
 const connection = new Redis(process.env.REDIS_URL, {
   maxRetriesPerRequest: null,
+  tls: {}, // ✅ Required for rediss://
 });
 
 (async () => {
-  await redis.set('foo', 'bar');
-  const value = await redis.get('foo');
+  await connection.set('foo', 'bar');
+  const value = await connection.get('foo');
   console.log('Redis test:', value); 
 })();
