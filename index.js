d7c9b50822bff2965c52dc4e9d8e1edb61f8430e
harshil62-cell <harshil2461@gmail.com>
2025-07-12 16:05:04 +0530

    fix: webhook fixed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const chatroomRoutes=require('./routes/chatroom');
 const paymentRoutes=require('./routes/payment');
 
 app.use(cors());
-app.use('/webhook/stripe', express.raw({ type: 'application/json' }), require('./routes/webhook'));
+app.use('/webhook', express.raw({ type: 'application/json' }), require('./routes/webhook'));
 app.use(express.json());
 
 // Routes will go here
