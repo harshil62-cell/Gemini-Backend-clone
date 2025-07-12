ac47ecfca6458765fa0f36a60ea49d041e546eb6
harshil62-cell <harshil2461@gmail.com>
2025-07-12 14:57:56 +0530

    fix: main file changed

--- queues/geminiQueue.js
+++ queues/geminiQueue.js
@@ -1,7 +1,10 @@
 const {Queue}=require('bullmq');
 const Redis=require('ioredis');
 
-const connection=new Redis();
+const connection = new Redis(process.env.REDIS_URL, {
+  maxRetriesPerRequest: null,
+  tls: {} // needed for `rediss://` secure connection
+});;
 
 const geminiQueue=new Queue('gemini',{connection});
 
